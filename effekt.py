2a2035d331b97f602229559efa50ac061d03aa90
linustux <linustux@users.noreply.github.com>
2015-12-17 20:13:38 +0100

    modified:   effekt.py

--- effekt.py
+++ effekt.py
@@ -9,12 +9,13 @@ UDP_PORT = 1337
 # Farben:
 
 weiss = "\xff\xff\xff"
+blau = "\x0b\x58\xc4"
 
 # CODE:
 
-for i in range(1,10000):
-	for l in range (1,16):
-		leiste = l-1
+# for i in range(1,10000):
+	#for l in range (1,16):
+		leiste = 4 
 		msg = ""
 		msg += chr(leiste)
 		msg += weiss * 112
