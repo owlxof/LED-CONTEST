f2760eca84c37bddaad6e0de1f81fbeeb04fab98
linustux <linustux@users.noreply.github.com>
2015-12-17 16:54:26 +0100

    modified:   README.md 	modified:   udp-sender.py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # LED-CONTEST
 my contribution to led contest from bastli
+
+todo: eine Leiste auf einmal.
+
+1. leiste auswählen (8 bit, wert 0 bis 14)
+
+2. farbwert einsetzen
+
+3. 
--- udp-sender.py
+++ udp-sender.py
@@ -1,15 +1,43 @@
-#!/ust/bin/python
+#!/usr/bin/python
 
+from time import sleep
 import socket
-   2 
-   3 UDP_IP = "10.6.66.10"
-   4 UDP_PORT = 1337
-   5 MESSAGE = "Hello, World!"
-   6 
-   7 print "UDP target IP:", UDP_IP
-   8 print "UDP target port:", UDP_PORT
-   9 print "message:", MESSAGE
-  10 
-  11 sock = socket.socket(socket.AF_INET, # Internet
-  12                      socket.SOCK_DGRAM) # UDP
-  13 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+UDP_IP = "10.6.66.10"
+UDP_PORT = 1337
+
+# wähle leiste
+leiste = "\x02"
+
+# black = hex(000000)
+# white = hex(ffffff)
+# blue = hex(0404d8)
+# orange = hex(ffd400)
+
+	s=1:15  # we have 15 LED strips
+MESSAGE=leiste
+for r in range(1,112):
+	MESSAGE+="\xff\xff\xff"
+while true: 
+		# use HSL mode, create a simple sign wave on hue, fixed values for
+        	#  saturation and luminance
+		# hue sat lum
+		# hsl = [0.5*(1+sin(2*pi*transp([1:N])/N + phi)) ones(N,1) 0.5*ones(N,1)]
+		# rbg = hsl2rgb(hsl); # convert to RGB
+	
+ 
+
+
+	# send mesage
+	sock = socket.socket(socket.AF_INET, # Internet
+	socket.SOCK_DGRAM) # UDP
+	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+
+
+
+# print "UDP target IP:", UDP_IP
+# print "UDP target port:", UDP_PORT
+# print "message:", MESSAGE
+
+# endoffile
