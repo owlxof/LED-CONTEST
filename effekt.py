b94ce0d818357143eee559cb37a66a2eef24bbc4
linustux <linustux@users.noreply.github.com>
2015-12-17 19:43:37 +0100

    new file:   effekt.py

---
+++ effekt.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import socket
+from time import sleep
+
+UDP_IP = "10.6.66.10"
+UDP_PORT = 1337
+
+# Farben:
+
+weiss = "\xff\xff\xff"
+
+# CODE:
+
+for i in range(1,10000):
+	for l in range (1,16):
+		leiste = l-1
+		msg = ""
+		msg += chr(leiste)
+		msg += weiss * 112
+
+
+		# send udp
+		sock = socket.socket(socket.AF_INET, # Internet
+		socket.SOCK_DGRAM) # UDP
+		sock.sendto(msg, (UDP_IP, UDP_PORT))
+	#sleep(0.1)
+
