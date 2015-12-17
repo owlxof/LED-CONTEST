04eac3d2df5f52e56cbd24c0dc3d0b0e10c55ec6
linustux <linustux@users.noreply.github.com>
2015-12-17 20:20:33 +0100

    modified:   effekt.py

--- effekt.py
+++ effekt.py
@@ -14,16 +14,16 @@ blau = "\x0b\x58\xc4"
 # CODE:
 
 # for i in range(1,10000):
-	#for l in range (1,16):
-		leiste = 4 
-		msg = ""
-		msg += chr(leiste)
-		msg += weiss * 112
+for l in range (1,16):
+ 	leiste = 3 
+ 	msg = ""
+ 	msg += chr(leiste)
+ 	msg += weiss * 112
 
 
-		# send udp
-		sock = socket.socket(socket.AF_INET, # Internet
-		socket.SOCK_DGRAM) # UDP
-		sock.sendto(msg, (UDP_IP, UDP_PORT))
-	#sleep(0.1)
+	# send udp
+ 	sock = socket.socket(socket.AF_INET, # Internet
+ 	socket.SOCK_DGRAM) # UDP
+ 	sock.sendto(msg, (UDP_IP, UDP_PORT))
+ 	#sleep(0.1)
 
