39dd3310d882ae8e9cb558ac8631efc145d9d4a4
linustux <linustux@users.noreply.github.com>
2015-12-18 18:07:00 +0100

    code säubern, mehr comments

--- effekt.py
+++ effekt.py
@@ -2,28 +2,36 @@
 
 import socket
 from time import sleep
+import colorsys
 
+# definiere UDP-Ziel:
 UDP_IP = "10.6.66.10"
 UDP_PORT = 1337
 
-# Farben:
+# hier evtl eine liste mit unterschiedlichen farben oder farbeffekte
+# einbauen
 
+
+# Farben:
 weiss = "\xff\xff\xff"
 blau = "\x0b\x58\xc4"
 
-# CODE:
 
-# for i in range(1,10000):
-for l in range (1,16):
- 	leiste = 3 
- 	msg = ""
+
+# für jede Leiste ein Packet, für jede LED 3 bytes (112 LEDs)
+
+# for i in range(1,10000000):
+for l in range (1,16): # über leisten iterieren
+ 	leiste = l-1
+	msg = ""
  	msg += chr(leiste)
- 	msg += weiss * 112
+ 	for led in range (1,112): #über leds iterieren
+	msg += blau * 112
 
 
-	# send udp
+	# send udp packet
  	sock = socket.socket(socket.AF_INET, # Internet
  	socket.SOCK_DGRAM) # UDP
  	sock.sendto(msg, (UDP_IP, UDP_PORT))
- 	#sleep(0.1)
-
+ 	# sleep(0.1)
+# endoffile
